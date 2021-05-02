281f95d836c728cd43f25920e5b2d149ee7d61af
pacm4n83 <kemkeut@gmail.com>
2021-05-02 15:18:52 +0100

    Blog v1.3

--- blog/urls.py
+++ blog/urls.py
@@ -1,13 +1,8 @@
 from django.urls import path
-from .views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView, SignUpView, \
-    add_comment_to_post
+from .views import BlogListView, BlogDetailView
+
 
 urlpatterns = [
-    path('accounts/signup/', SignUpView.as_view(), name="signup"),
-    path('post/<int:pk>/delete/', BlogDeleteView.as_view(), name="post_delete"),
-    path('post/<int:pk>/edit/', BlogUpdateView.as_view(), name="post_edit"),
-    path('post/new/', BlogCreateView.as_view(), name="post_new"),
-    path('post/<int:pk>/', BlogDetailView.as_view(), name="post_detail"),
-    path('post/<int:pk>/comment/', add_comment_to_post.as_view(), name="add_comment_to_post"),
-    path('', BlogListView.as_view(), name="home"),
+    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
+    path('', BlogListView.as_view(), name='home'),
 ]
